dd810904b7a3178d422c958fa776807040336e43
Megapreneur <ademolamegbabi@gmail.com>
2022-07-13 19:33:26 +0100

    School Management App

--- src/main/java/africa/semicolon/data/model/Staff.java
+++ src/main/java/africa/semicolon/data/model/Staff.java
@@ -1,6 +1,17 @@
 package africa.semicolon.data.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Staff {
+    @Id
+
+    private String id;
     private String name;
     private String phoneNumber;
     private String address;
--- src/main/java/africa/semicolon/data/model/Student.java
+++ src/main/java/africa/semicolon/data/model/Student.java
@@ -1,12 +1,17 @@
 package africa.semicolon.data.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class Student {
-
     private String name;
+    @Id
     private String id;
     private String grade;
     private double totalFees;
