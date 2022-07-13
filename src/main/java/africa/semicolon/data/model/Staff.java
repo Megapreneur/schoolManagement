8b05245ba478b053c44143bb636805073b5e1f09
Megapreneur <ademolamegbabi@gmail.com>
2022-07-13 19:17:58 +0100

    School Management App

--- src/main/java/africa/semicolon/data/model/Staff.java
+++ src/main/java/africa/semicolon/data/model/Staff.java
@@ -1,4 +1,11 @@
 package africa.semicolon.data.model;
 
 public class Staff {
+    private String name;
+    private String phoneNumber;
+    private String address;
+    private String email;
+    private String gender;
+    private int age;
+    private StaffType type;
 }
---
+++ src/main/java/africa/semicolon/data/model/StaffType.java
@@ -0,0 +1,5 @@
+package africa.semicolon.data.model;
+
+public enum StaffType {
+    TEACHING, NON_TEACHING
+}
