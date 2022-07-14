a804eb463352def2432746cf111e0425bc8f4107
Megapreneur <ademolamegbabi@gmail.com>
2022-07-14 15:52:50 +0100

    School Management App

--- src/main/java/africa/semicolon/controller/StudentController.java
+++ src/main/java/africa/semicolon/controller/StudentController.java
@@ -1,4 +1,19 @@
 package africa.semicolon.controller;
 
+import africa.semicolon.Dto.Requests.RegisterRequest;
+import africa.semicolon.Dto.Response.RegisterResponse;
+import africa.semicolon.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class StudentController {
+    @Autowired
+    private StudentService studentService;
+    @PostMapping("/student")
+    public RegisterResponse register(@RequestBody RegisterRequest registerRequest){
+        return studentService.register(registerRequest);
+    }
 }
